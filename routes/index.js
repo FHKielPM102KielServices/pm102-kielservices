d2d6920b7c37b0d8a6c90cdde057ce1feb46c8e3
kavyabellamkonda <sai.k.bellamkonda@student.fh-kiel.de>
2016-11-22 15:43:52 +0100

    attached login page in home

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,16 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
+
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Kiel Services Web Application' });
+    res.render('index', { title: 'Kiel Services Web Application' });
+});
+/* GET home page. */
+router.get('/login', function(req, res, next) {
+    res.render('login', { title: 'Kiel Services Web Application' });
+});
+router.get('/forgotpassword', function(req, res, next) {
+    res.render('forgotpassword', { title: 'Kiel Services Web Application' });
 });
 
 module.exports = router;
--- routes/users.js
+++ routes/users.js
@@ -5,5 +5,4 @@ var router = express.Router();
 router.get('/', function(req, res, next) {
   res.send('respond with a resource');
 });
-
 module.exports = router;
