dfe79d8aa60629233104444c4bbfc9b9523c81a2
Sarathkumarmohan <sarath.k.mohan@student.fh-kiel.de>
2016-11-28 12:37:46 +0100

    Correcting error
    Signed-off-by: Sarathkumarmohan <sarath.k.mohan@student.fh-kiel.de>

--- routes/index.js
+++ routes/index.js
@@ -9,7 +9,7 @@ var mysql = require("mysql");
 var con = mysql.createConnection({
     host: "localhost",
     user: "root",
-    password: "1234",``
+    password: "1234",
     database: "mydb"
 });
 
