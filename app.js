ba238593750c186910ca302cf3767e1ff47c6549
Maryam <Maryam.eghbali2015@gmail.com>
2016-11-30 00:24:42 +0100

    Rejected commit: Default

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ var bodyParser = require('body-parser');
 var index = require('./routes/index');
 var users = require('./routes/users');
 var ShowNearPlace = require('./routes/ShowNearPlace');
+var SubjectsList = require('./routes/SubjectsList');
 
 var app = express();
 
@@ -26,6 +27,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', index);
 app.use('/users', users);
 app.use('/ShowNearPlace', ShowNearPlace);
+app.use('/SubjectsList', SubjectsList);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
---
+++ routes/SubjectsList.js
@@ -0,0 +1,13 @@
+
+var express = require('express');
+var router = express.Router();
+
+/* GET ShowNearPlace page. */
+
+
+
+router.get('/', function(req, res, next) {
+    res.render('SubjectsList',{ title: 'Services' });
+});
+
+module.exports = router;
