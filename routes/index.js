338c846590a3df06d34dafe3f7a8e802cd7703cb
mahdi.ehsanifar <mahdi.ehsanifar@gmail.com>
2016-11-30 22:16:07 +0100

    merge problem

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,9 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Kiel Services Web Application' });
 });
 
+/* GET home page. */
+router.get('/login', function(req, res, next) {
+    res.render('login', { title: 'Kiel Services Web Application' });
+});
+
 module.exports = router;
