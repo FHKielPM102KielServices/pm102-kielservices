3e81285d67b87ff68d89f47b8b0517008c791ee6
Raghava <kannaraghava@gmail.com>
2016-12-14 19:20:06 +0100

    (Task) : Websockets
    Signed-off-by: Raghava <kannaraghava@gmail.com>

--- app.js
+++ app.js
@@ -10,12 +10,65 @@ var userDashboard = require('./routes/userDashboard');
 var ShowNearPlace = require('./routes/ShowNearPlace');
 var SubjectsList = require('./routes/SubjectsList');
 var Addreview = require('./routes/Addreview');
-
+//var chat_box = require('./routes/chat_server');
 
 require('./routes/extensions.js');
 
 var app = express();
+app.set('port', process.env.PORT || 3000);
 
+var httpServer = require('http').createServer(app);
+var io = require('socket.io')(httpServer);
+//var people = {};
+//var usernames = {};
+//connections = [];
+var usernames = {};
+
+// rooms which are currently available in chat
+var rooms = ['room1','room2','room3'];
+//var people = {};
+
+io.on('connection', function (socket) {
+// when the client emits 'adduser', this listens and executes
+    socket.on('adduser', function(username){
+        // store the username in the socket session for this client
+        socket.username = username;
+        // store the room name in the socket session for this client
+        socket.room = 'room1';
+        // add the client's username to the global list
+        usernames[username] = username;
+        // send client to room 1
+        socket.join('room1');
+        // echo to client they've connected
+        socket.emit('updatechat', 'KS_SERVER', 'you have connected to Kiel Services Administration');
+        // echo to room 1 that a person has connected to their room
+        socket.broadcast.to('room1').emit('updatechat', 'KS_SERVER', username + ' has connected to this room');
+        socket.emit('updaterooms', rooms, 'room1');
+    });
+
+    // when the client emits 'sendchat', this listens and executes
+    socket.on('sendchat', function (data) {
+        // we tell the client to execute 'updatechat' with 2 parameters
+        io.sockets.in(socket.room).emit('updatechat', socket.username, data);
+    });
+
+
+    // when the user disconnects.. perform this
+    socket.on('disconnect', function(){
+        // remove the username from global usernames list
+        delete usernames[socket.username];
+        // update list of users in chat, client-side
+        io.sockets.emit('updateusers', usernames);
+        // echo globally that this client has left
+        socket.broadcast.emit('updatechat', 'SERVER', socket.username + ' has disconnected');
+        socket.leave(socket.room);
+    });
+});
+
+httpServer.listen(app.get('port'), function() {
+    console.log('Express server listening on port ' + app.get('port'));
+});
+//app.io = require('socket.io')();
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'hjs');
@@ -29,11 +82,12 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', index);
+// app.use('/users', users);
 app.use('/userDashboard', userDashboard);
 app.use('/ShowNearPlace', ShowNearPlace);
 app.use('/SubjectsList', SubjectsList);
 app.use('/Addreview', Addreview);
-
+//app.use('/chat_box', chat_box);
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   var err = new Error('Not Found');
--- routes/index.js
+++ routes/index.js
@@ -36,6 +36,14 @@ router.get('/contactUs', function(req, res, next) {
     res.render('contactUs', { title: 'Kiel Services Web Application' });
 });
 router.use(session({secret:"sdsdsdsds",saveUninitialized:true,resave:true}))
+router.get('/', function(req, res, next) {
+    res.render('chatbox', { title: 'Express' });
+});
+
+//
+// router.get('/chat_box', function(req, res, next) {
+//     res.render('chat_box', { title: 'Kiel Services Web Application'});
+// });
 router.use(passport.initialize());
 router.use(passport.session());
 router.post('/login', function(req, res, next) {
@@ -147,4 +155,91 @@ router.get('/logout', function (req, res) {
     //res.send("logout success!");
     res.redirect('/');
 });
+
+router.get('/chat_box', function(req, res, next) {
+    res.render('chat_box', { title: 'Kiel Services Web Application' });
+});
+
+//Chat Box
+//this line not needed remove it
+//var http = require('http');
+//var app = express();
+//before this inirialise app (var app = express());
+//app.set('port', process.env.PORT || 3000);
+
+//this linw is necessary keep as it is
+//var httpServer = require('http').createServer(app);
+//var server = http.createServer(app);
+//u pass server from line 150 to io, and not http variable
+//ur original code       var io = require('socket.io')(http);
+//wht it shd look like
+//var io = require('socket.io')(httpServer);
+
+//remove
+
+
+//remove
+//var io = require('socket.io').listen(server);
+//var io = require('socket.io').listen(server);
+
+
+//for listening u do something like
+/*httpServer.listen(app.get('port'), function() {
+    console.log('Express server listening on port ' + app.get('port'));
+});
+console.log('socket Server Running');*/
+
+router.get('/', function(req, res){
+    res.sendFile(__dirname +'chat_box');
+});
+
+//io serverside code , this is important!!
+/*
+io.on('connection', function(socket){
+    //instead of chatMessage & notifyUser,u can use your own events from index.html
+    connections.push(socket);
+    console.log('connected: %s sockets connected', connections.length);
+
+    Disconnect
+    socket.on('disconnect', function(data){
+        connections.splice(connections.indexOf(socket),1);
+        console.log('disconnected: %s sockets connected', connections.length);
+    });
+
+    socket.on('send message',function(data){
+        console.log(data);
+        io.sockets.emit('new message',{msg: data});
+    })
+
+*/
+//});
+
+
+// router.get('/chat_box', function(req, res, next) {
+//     res.render(__dirname +'/chat_box.hjs');
+// });
+
+
+//i dont know if u can also write io.sockets.on!!
+// io.sockets.on('connection', function(socket){
+//     connections.push(socket);
+//     console.log('connected: %s sockets connected', connections.length);
+//
+//     Disconnect
+//     socket.on('disconnect', function(data){
+//         connections.splice(connections.indexOf(socket),1);
+//         console.log('disconnected: %s sockets connected', connections.length);
+//     });
+//
+//     socket.on('send message',function(data){
+//         console.log(data);
+//         io.sockets.emit('new message',{msg: data});
+//     })
+//
+// });
+
+//no longer needed
+// server.listen(3000, function(){
+//     console.log('listening on *:3000');
+// });
 module.exports = router;
